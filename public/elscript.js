6e0cadb0273528618ff37cf043c192eb8c01c81d
isaelfeliciano <rogelioisael@hotmail.com>
2014-03-21 22:37:45 -0400

    init

---
+++ public/elscript.js
@@ -0,0 +1,11 @@
+$(document).ready(inicio);
+	function inicio(){
+		$('input[name="convertir"]').on("click", function(){
+			var entrada_numero = $('input[name="entrada_numero"]').val();
+			var salida_numero = leerNumeros(entrada_numero);
+			if($('input[name="salida_numero"]').val(salida_numero) !== ""){
+				$('textarea[name="salida_numero"]').val("");
+			}
+			$('input[name="salida_numero"]').val(salida_numero);
+		});
+	}
